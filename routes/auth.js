a310788aa060134bccf6610fcb13ceffd0d49d60
Daniel <gtzfdaniel@gmail.com>
2023-06-13 10:52:27 -0600

    Fin sección 11 - version 4.0.0

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 
 
-const { login } = require('../controllers/auth');
+const { login, googleSignIn } = require('../controllers/auth');
 const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
@@ -12,8 +12,12 @@ router.post('/login',[
     check('correo', 'El correo es obligatorio').isEmail(),
     check('password', 'La contraseña es obligatoria').not().isEmpty(),
     validarCampos
-],
- login);
+], login);
+
+router.post('/google',[
+    check('id_token', 'id_token es necesario').not().isEmpty(),
+    validarCampos
+], googleSignIn);
 
 
 module.exports = router;
